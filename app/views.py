c00b016050b42394296d2e4657ebd7d8a1e5c205
alizauf <aliza.aufrichtig@gmail.com>
2016-03-20 12:57:02 -0400

    makin some progress

--- app/views.py
+++ app/views.py
@@ -6,6 +6,17 @@ from app import app
 def index():
     return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/writing')
+def writing():
+    return render_template('writing.html')
+
+@app.route('/design')
+def design():
+    return render_template('design.html')
 
 @app.route('/twin-peaks')
 def twin_peaks():
